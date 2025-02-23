44ad468c91480bf73bc38448521337352e388e43
Robert Lin <45280066+r002@users.noreply.github.com>
2025-02-22 20:59:02 -0500

    close #17: revamp the nav bar

--- js/navbar.js
+++ js/navbar.js
@@ -1,14 +1,35 @@
-fetch("../data/navbar.json")
-    .then(rs=>rs.json()
-    .then(navbaritems => {
-        const navbar = [];
-        for (item of navbaritems) {
-            if (item?.path !== window.location.pathname) {
-                navbar.push(
-                    `<a href="${item.url}">${item.title}</a>`
-                );
-            }
-        }
-        // console.table(navbar);
-        document.getElementById("navbar").innerHTML = navbar.join(" • ");
-}));
+function changeNavbar(secId) {
+    // console.log(">> fire changeNavbar", section);
+
+    document.querySelectorAll('.section').forEach((el) => {
+      el.classList.remove('selectedsection');
+    });
+    document.getElementById(secId).classList.add('selectedsection');
+
+    if (secId == "blueskySec") {
+      document.getElementById('nav1').style.display = 'block';
+      document.getElementById('nav2').style.display = 'none';
+      document.getElementById('nav4').style.display = 'none';
+    } else if (secId == "experimentsSec") {
+      document.getElementById('nav1').style.display = 'none';
+      document.getElementById('nav2').style.display = 'block';
+      document.getElementById('nav4').style.display = 'none';
+    } else if (secId == "miscSec") {
+      document.getElementById('nav1').style.display = 'none';
+      document.getElementById('nav2').style.display = 'none';
+      document.getElementById('nav4').style.display = 'block';
+    }
+}
+
+// Select the correct navbar item based on the current URL
+function showDefaultNavbar() {
+    // console.log(">> fire showSelectedNavbar", new Date());
+    const url = window.location.pathname;
+    if (url.includes("ask") || url.includes("maze") || url.includes("bmc")) {
+        changeNavbar('experimentsSec');
+    } else {
+        changeNavbar('blueskySec');
+    }
+}
+
+showDefaultNavbar()
