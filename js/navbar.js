65a00e0a317e8c2fe369537ea516065eaada453b
Robert Lin <45280066+r002@users.noreply.github.com>
2025-03-02 19:38:19 -0500

    add `kingdom` page + footer

--- js/navbar.js
+++ js/navbar.js
@@ -9,14 +9,22 @@ function changeNavbar(secId) {
     if (secId == "blueskySec") {
       document.getElementById('nav1').style.display = 'block';
       document.getElementById('nav2').style.display = 'none';
+      document.getElementById('nav3').style.display = 'none';
       document.getElementById('nav4').style.display = 'none';
     } else if (secId == "experimentsSec") {
       document.getElementById('nav1').style.display = 'none';
       document.getElementById('nav2').style.display = 'block';
+      document.getElementById('nav3').style.display = 'none';
+      document.getElementById('nav4').style.display = 'none';
+    } else if (secId == "kingdomSec") {
+      document.getElementById('nav1').style.display = 'none';
+      document.getElementById('nav2').style.display = 'none';
+      document.getElementById('nav3').style.display = 'block';
       document.getElementById('nav4').style.display = 'none';
     } else if (secId == "miscSec") {
       document.getElementById('nav1').style.display = 'none';
       document.getElementById('nav2').style.display = 'none';
+      document.getElementById('nav3').style.display = 'none';
       document.getElementById('nav4').style.display = 'block';
     }
 }
@@ -27,6 +35,8 @@ function showDefaultNavbar() {
     const url = window.location.pathname;
     if (url.includes("ask") || url.includes("maze") || url.includes("bmc") || url.includes("gbc")) {
         changeNavbar('experimentsSec');
+    } else if (url.includes("kingdom")) {
+      changeNavbar('kingdomSec');
     } else {
         changeNavbar('blueskySec');
     }
